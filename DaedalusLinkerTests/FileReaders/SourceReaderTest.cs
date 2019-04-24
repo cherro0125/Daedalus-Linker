648427242df63d2086afa5f1f5f2d4aa4b866d3d
Cherro <kaliszewski.dominik@gmail.com>
2019-04-24 22:29:34 +0200

    Add SourceReader test

---
+++ DaedalusLinkerTests/FileReaders/SourceReaderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DaedalusLinker.FileReaders;
+using DaedalusLinker.Exceptions;
+using NUnit.Framework;
+
+namespace DeadalusLinkerTests.FileReaders
+{
+    [TestFixture]
+    public class SourceReaderTest
+    {
+        [Test]
+        public void SourceReaderWithBadExtension()
+        {
+            try
+            {
+                SourceReader sr = new SourceReader("simple.txt");
+                Assert.Fail();
+            }
+            catch (BadExtensionException ex)
+            {
+                Assert.Pass();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
